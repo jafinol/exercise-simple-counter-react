9598e524df55d3bb73a4d4c1887d61655ab533a4
jafinol <jafinol@gmail.com>
2021-02-18 02:43:03 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,84 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
+	const [time, setTime] = useState(0);
+	const [time2, setTime2] = useState(0);
+	const [time3, setTime3] = useState(0);
+	const [time4, setTime4] = useState(0);
+	const [time5, setTime5] = useState(0);
+	const [time6, setTime6] = useState(0);
+
+	useEffect(() => {
+		setInterval(() => {
+			setTime(time => time + 1);
+		}, 1000);
+	}, []);
+
+	if (time === 10) {
+		setTime(0);
+		setTime2(time2 + 1);
+	}
+
+	if (time2 === 10) {
+		setTime2(0);
+		setTime3(time3 + 1);
+	}
+
+	if (time3 === 10) {
+		setTime3(0);
+		setTime4(time4 + 1);
+	}
+
+	if (time4 === 10) {
+		setTime4(0);
+		setTime5(time5 + 1);
+	}
+
+	if (time5 === 10) {
+		setTime5(0);
+		setTime6(time6 + 1);
+	}
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="row justify-content-center ">
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>
+						<i className="far fa-clock"></i>
+					</h1>
+				</div>
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>{time6}</h1>
+				</div>
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>{time6}</h1>
+				</div>
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>{time5}</h1>
+				</div>
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>{time4}</h1>
+				</div>
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>{time3}</h1>
+				</div>
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>{time2}</h1>
+				</div>
+
+				<div className="col-1 card bg-dark" style={{ width: " 18rem" }}>
+					<h5 className="card-title"></h5>
+					<h1 style={{ color: "white" }}>{time}</h1>
+				</div>
+			</div>
 		</div>
 	);
 }
